2ee00b5bfe9f5a6e24664bac7b60158d00150658
Walter Molobela <waltermolobela@gmsil.com>
2018-01-12 10:14:16 +0200

    Changed login layout

---
+++ InterviewAssigment/Utils/Rest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace InterviewAssigment.Utils
+{
+    public class Rest
+    {
+        public string Get(string uri)
+        {
+            var result = RunAsync(uri).Result;
+            //Validate(result, uri);
+            return result;
+        }
+
+        private async Task<string> RunAsync(string uri)
+        {
+            try
+            {
+                return "";
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
